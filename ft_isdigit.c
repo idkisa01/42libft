7cd88c9c4fe8b70841f5be1da8f909afdc1614db
isabel <idkisa276@gmail.com>
2024-09-17 19:54:40 +0200

    all

---
+++ ft_isdigit.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isabegar <isabegar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 18:15:39 by isabegar          #+#    #+#             */
+/*   Updated: 2024/09/17 19:30:30 by isabegar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//#include <stdio.h>
+
+int	ft_isdigit(int digit)
+{
+	if (digit >= '0' && digit <='9')
+		return (1);
+	else
+	return (0);
+}
+
+/*int	main(void)
+{
+	char digit = '2';
+	printf("%d", ft_isdigit (digit));
+	return(0);
+}*/
