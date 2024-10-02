72c98bb42234ecab37b05551cdc04dec83d63008
isabel <idkisa276@gmail.com>
2024-10-02 14:30:02 +0200

    add all

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isabegar <isabegar@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-10-02 11:53:34 by isabegar          #+#    #+#             */
+/*   Updated: 2024-10-02 11:53:34 by isabegar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t		i;
+	const unsigned char	*ptr_s1;
+	const unsigned char	*ptr_s1;
+
+	ptr_s1 = (const unsigned char *)s1;
+	ptr_s2 = (const unsigned char *)s2;
+	i = 0;
+	while ((ptr_s1[i] != '\0' || ptr_s2[i] != '\0') && i < n)
+	{
+		if (ptr_s1[i] != ptr_s2[i])
+			return (ptr_s1[i] - ptr_s2[i]);
+		i++;
+	}
+	return (0);
+}
