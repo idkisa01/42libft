ecb1d895d96a89d0a97510682feec03cdabefac6
isabel <idkisa276@gmail.com>
2024-10-11 19:14:45 +0200

    add split.c

--- ft_split.c
+++ ft_split.c
@@ -10,5 +10,71 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
-char	**ft_split(const char *s, char c)
+static	size_t	lenght_word(char const *s, char c)
+{
+	size_t	size;
+
+	size = 0;
+	while (s[size] && s[size] != c)
+		size++;
+	return (size);
+}
+
+static	size_t	count_words(char const *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			count++;
+			s += lenght_word(s, c);
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+static	void	free_mem(char **words)
+{
+	size_t	i;
+
+	i = 0;
+	while (words[i])
+		free(words[i++]);
+	free(words);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**words;
+	size_t	ntwords;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	ntwords = count_words(s, c);
+	words = (char **)malloc((ntwords + 1) * sizeof(char *));
+	if (!words)
+		return (NULL);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			words[i] = ft_substr(s, 0, lenght_word(s, c));
+			if (!words[i++])
+				return (free_mem(words), NULL);
+			s += lenght_word(s, c);
+		}
+		else
+			s++;
+	}
+	words[i] = NULL;
+	return (words);
+}
